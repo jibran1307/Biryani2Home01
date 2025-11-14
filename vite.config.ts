8758b8d17dd965b571a72edd45a56e1b1c9e182a
jibran <84091440+jibran1307@users.noreply.github.com>
2025-11-15 00:46:11 +0530

    Refactor Vite config to simplify server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react()
-  ],
+export default defineConfig({
+  base: "/",  // MUST BE THIS FOR CUSTOM DOMAIN
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
